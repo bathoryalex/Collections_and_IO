4572e60de5f91f1ccf534c62f84a4048950c8130
Bathory Alex <bathory.alex@gmail.com>
2016-04-09 19:02:45 +0200

    Completion of Exercise 4

---
+++ Collections/Exercise_4/LookupCollections/LookupCollections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace LookupCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+
+            list["Estados Unidos"] = "United States of America";
+            list["Canadá"] = "Canada";
+            list["España"] = "Spain";
+
+            Console.WriteLine(list["españa"]);
+            Console.WriteLine(list["CANADÁ"]);
+            Console.Read();
+        }
+    }
+}
